fc9a7b829ad394fd4f1f5d99e1614b8615f296b7
Sara Bailouni <sbailouni@ucsd.edu>
2024-10-08 23:50:20 -0700

    Exercise 4.1 Done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,30 @@
 import './App.css';
+import { Label, Note } from "./types"; // Import the Label type from the appropriate module
+import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
 
 function App() {
   return (
     <div className='app-container'>
-      <div className="note-individual">
-        <button>
-          x
-        </button>
-        <h2> 1st Note Title </h2>
-        <p> 1st Note Content </p>
-        <p> 1st Note Label </p>
+      <form className="note-form">
+        <div><input placeholder="Note Title"></input></div>
+
+        <div><textarea></textarea></div>
+
+        <div><button type="submit">Create Note</button></div>
+      </form>
+      <div className="notes-grid">
+        {dummyNotesList.map((note) => (
+          <div
+            key={note.id}
+            className="note-item">
+            <div className="notes-header">
+              <button>x</button>
+            </div>
+            <h2> {note.title} </h2>
+            <p> {note.content} </p>
+            <p> {note.label} </p>
+          </div>
+        ))}
       </div>
     </div>
   );
